2d9dd2871357178684cfb9ee7439105885157202
liamheidenberg <142893205+liamheidenberg@users.noreply.github.com>
2024-01-16 14:01:04 +0100

    dark mode

--- script.js
+++ script.js
@@ -26,3 +26,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function colormode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
